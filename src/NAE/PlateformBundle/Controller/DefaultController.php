e81e32f321fb95f677a0f85157afbd50648135b2
Juba95 <juba.oumaza@gmail.com>
2014-10-08 15:19:21 +0200

    debut de synfony

---
+++ src/NAE/PlateformBundle/Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace NAE\PlateformBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction()
+    {
+        return $this->render('NAEPlateformBundle:Default:index.html.twig');
+    }
+}
---
+++ src/NAE/PlateformBundle/NAEPlateformBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace NAE\PlateformBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class NAEPlateformBundle extends Bundle
+{
+}
