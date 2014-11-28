7a862952910dd30214e238d6faf951fe581dd821
Juba95 <juba.oumaza@gmail.com>
2014-11-28 11:01:29 +0100

    class admin sonata

--- README.md
+++ README.md
@@ -4,5 +4,5 @@ NewArtExpo
 #Admin 
 
 	*Installation de la partie Admin -> good
-	*Récupérer la BDD en générant les entités -> en cours
-	*
+	*Récupérer la BDD en générant les entités -> Good
+	*Création de la class Admin
--- src/NAE/PlateformBundle/DependencyInjection/NAEPlateformExtension.php
+++ src/NAE/PlateformBundle/DependencyInjection/NAEPlateformExtension.php
@@ -5,6 +5,7 @@ namespace NAE\PlateformBundle\DependencyInjection;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+
 use Symfony\Component\DependencyInjection\Loader;
 
 /**
@@ -23,6 +24,8 @@ class NAEPlateformExtension extends Extension
         $config = $this->processConfiguration($configuration, $configs);
 
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+
         $loader->load('services.yml');
+        $loader->load('admin.yml');
     }
 }
