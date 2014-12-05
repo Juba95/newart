afb8fcea6c74d9361d83be88afee828ebc715767
Mark1Mat <matissevlp@gmail.com>
2014-12-05 19:57:38 +0100

    remise en propre matisse

--- src/NAE/PlateformBundle/Controller/ExpoController.php
+++ src/NAE/PlateformBundle/Controller/ExpoController.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 
 // src/NAE/PlatformBundle/Controller/ExpoController.php
@@ -6,7 +5,7 @@
 namespace NAE\PlateformBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Form\Extension\HttpFoundation\reponse;
 
 class ExpoController extends Controller
 {
@@ -50,60 +49,4 @@ class ExpoController extends Controller
         return $this->render('NAEPlateformBundle:Expo:article.html.twig');
     }
 
-    public function cvgAction()
-    {
-        return $this->render('NAEPlateformBundle:Expo:cvg.html.twig');
-    }
-=======
-<?php
-
-// src/NAE/PlatformBundle/Controller/ExpoController.php
-
-namespace NAE\PlateformBundle\Controller;
-
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Response;
-
-class ExpoController extends Controller
-{
-    public function indexAction()
-    {
-         return $this->render('NAEPlateformBundle:Expo:index.html.twig');
-    }
-
-    public function profilAction()
-    {
-        return $this->render('NAEPlateformBundle:Expo:profil.html.twig');
-    }
-
-    public function booksAction()
-    {
-        return $this->render('NAEPlateformBundle:Expo:books.html.twig');
-    }
-
-    public function expoAction()
-    {
-        return $this->render('NAEPlateformBundle:Expo:expo.html.twig');
-    }
-
-    public function naeAction()
-    {
-        return $this->render('NAEPlateformBundle:Expo:nae.html.twig');
-    }
-
-    public function contactAction()
-    {
-        return $this->render('NAEPlateformBundle:Expo:contact.html.twig');
-    }
-
-    public function articlesAction()
-    {
-        return $this->render('NAEPlateformBundle:Expo:articles.html.twig');
-    }
-
-    public function articleAction()
-    {
-        return $this->render('NAEPlateformBundle:Expo:article.html.twig');
-    }
->>>>>>> FETCH_HEAD
 }
