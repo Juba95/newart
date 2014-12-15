da3a8e61fb54d81eb1f39fca69d697f2ea701b6c
Mark1Mat <matissevlp@gmail.com>
2014-12-16 00:05:50 +0100

    Intégration Bundle NewsLetter + Update Massiv composer.json

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,9 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new NAE\PlateformBundle\NAEPlateformBundle(),
+            new Wowo\QueueBundle\WowoQueueBundle(),
+            new Wowo\NewsletterBundle\WowoNewsletterBundle(),
+
 
 
             //Sonata
