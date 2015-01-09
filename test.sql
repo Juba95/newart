09e2846fe654b0dc43672d60ec9b547d038eb52c
Juba95 <juba.oumaza@gmail.com>
2015-01-09 21:48:53 +0100

    debug bdd

--- test.sql
+++ test.sql
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 SET FOREIGN_KEY_CHECKS = 0;
 ALTER TABLE news_entry DROP title, CHANGE text text VARCHAR(200) DEFAULT NULL;
 ALTER TABLE sf_guard_remember_key DROP INDEX UNIQ_1EEA28AA76ED395, ADD INDEX IDX_1EEA28AA76ED395 (user_id);
@@ -9,7 +8,6 @@ ALTER TABLE sf_guard_user CHANGE email email VARCHAR(256) NOT NULL;
 ALTER TABLE user CHANGE account_id account_id INT NOT NULL;
 DROP INDEX idx_8d93d6499b6b5fba ON user;
 CREATE INDEX user_FI_1 ON user (account_id);
-SET FOREIGN_KEY_CHECKS = 1;
-=======
 ALTER TABLE user ADD CONSTRAINT FK_8D93D6499B6B5FBA FOREIGN KEY (account_id) REFERENCES sf_guard_user (id);
->>>>>>> steeve
+SET FOREIGN_KEY_CHECKS = 1;
+
